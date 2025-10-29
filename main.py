3968b6720beec8daa252d65079088284bfddac3d
Роман Новодворський web2025-3 <novodvorskyiroma@gmail.com>
2025-10-29 15:44:33 +0200

    Зчитування та підготовка даних

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.integrate import quad, simpson, trapezoid
 
-#yurii marchak test changes
+df = pd.read_csv("../index_1.csv")
+df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
+df = df.dropna(subset=['datetime'])
+df['money'] = df['money'].astype(float)
 
+#групуємо по хвилинах
+data = df.groupby(df['datetime']).agg(
+    S=('coffee_name', 'count'),
+    Revenue=('money', 'sum')
+)
+data['price'] = data['Revenue'] / data['S']
